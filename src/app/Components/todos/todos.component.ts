2b1ebb05a3f01eec87ceb796572ae9630cf89f7d
imshubham <jangleshubham@ymail.com>
2021-07-03 19:59:00 +0530

    Stable: Local Storage added

--- src/app/Components/todos/todos.component.ts
+++ src/app/Components/todos/todos.component.ts
@@ -7,30 +7,16 @@ import { Todo } from '../../Model/Todo'
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
-  todos: Todo[];
+  localItem: string;
+  todos!: Todo[];
 
   constructor() {
-    this.todos = [
-      {
-        sno: 1,
-        title: "This is title 1",
-        desc: "This is the desc 1",
-        active: false
-      },
-      {
-        sno: 2,
-        title: "This is title 2",
-        desc: "This is the desc 21",
-        active: false
-      },
-      {
-        sno: 3,
-        title: "This is title 3",
-        desc: "This is the desc 333",
-        active: false
-      },
-    ]
+    this.localItem = localStorage.getItem("todos") || "";
+    if(this.localItem.length == 0){
+      this.todos = []
+    } else {
+      this.todos = JSON.parse(this.localItem)
+    }
   }
 
   ngOnInit(): void {
@@ -39,10 +25,16 @@ export class TodosComponent implements OnInit {
   deleteFunction(todo: Todo): void {
     const index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);    
+
+    // save to local storage
+    localStorage.setItem("todos", JSON.stringify(this.todos));
   }
 
   addFunction(todo: Todo): void {
     this.todos.push(todo);
+
+    // save to local storage
+    localStorage.setItem("todos", JSON.stringify(this.todos));
   }
 
 }
